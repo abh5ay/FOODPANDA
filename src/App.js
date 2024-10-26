b00d2f3d9150d3de91765df3b4ece1eb1b690c62
Abhay <146737497+abh5ay@users.noreply.github.com>
2024-10-26 17:28:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+
 import Navbar from './components/Navbar';
 import RestaurantList from './components/RestaurantList';
 import Footer from './components/Footer';
